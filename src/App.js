066789282e14aa9651312288031dc61166ebcfd1
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2023-12-31 00:18:40 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is Cloud note beook</h1>
-    </div>
+    <>
+      {/* <h1>this is Cloud note beook</h1> */}
+      <Router>
+      <Navbar/>
+        <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        </Routes>
+        <Routes>
+        <Route exact path="/about" element={<About/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
